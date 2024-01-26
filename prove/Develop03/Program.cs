439032834e3528d9854a1e4d7988ef1c675cf4f4
oswell-nii <oswellokine1999@gmail.com>
2024-01-26 17:06:50 +0000

    Scripture Memorizer

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        var singleVerseReference = new Scripture(new Reference("John", 3, 16), "For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life.");
+        var verseRangeReference = new Scripture(new Reference("Proverbs", 3, 5, 6),"Trust in the LORD with all your heart and lean not on your own understanding; In all your ways acknowledge him, and he will make your paths straight.");
+        
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to our Scripture Memorizer Program!\n");
+            Console.WriteLine(verseRangeReference.GetDisplayText());
+
+            if (verseRangeReference.HasVisibleWords())
+            {
+                Console.WriteLine("\nPress Enter to continue or type 'quit' to quit the program");
+                string userInput = Console.ReadLine();
+
+                if (userInput == "quit")
+                {
+                    break;
+                }  
+                
+                verseRangeReference.HideRandomWords(1);
+
+            } 
+            else 
+            {
+                Console.WriteLine("\nPress Enter to continue or type 'quit' to quit the program");
+                string userInput = Console.ReadLine();
+                break;
+                
+            }
+
+        }    
     }
 }
---
+++ prove/Develop03/Reference.Cs
@@ -0,0 +1,34 @@
+public class Reference
+{
+    public string _book;
+    public int _chapter;
+    public int _verse;
+    public int _endVerse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+    }
+
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = startVerse;
+        _endVerse = endVerse;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_endVerse == 0)
+        {
+            return $"{_book} {_chapter}:{_verse}";
+        }
+        else
+        {
+            return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        }
+    }
+}
