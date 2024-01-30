fd46a625f9c443325d6b635cb6bcc30e9d99bf2d
oswell-nii <oswellokine1999@gmail.com>
2024-01-30 09:10:43 +0000

    Assignments Program

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "Causes of World War");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInfo());
+
     }
 }
